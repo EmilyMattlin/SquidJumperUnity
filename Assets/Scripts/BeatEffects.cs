3ceb5d9871153826977bb04eb229040d58cb4e4c
EmilyMattlin <emilyjmattlin@gmail.com>
2020-11-25 01:54:52 -0500

    Movement and preliminary beat detection

---
+++ Assets/Scripts/BeatEffects.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatEffects : MonoBehaviour
+{
+	private int i;
+	private bool active;
+	public GameObject rightWall;
+	public GameObject leftWall;
+	// Start is called before the first frame update
+	void Start()
+    {
+		transform.position += new Vector3(0f, 0f, 0.5f);
+		active = true;
+		i = 1;
+		//Select the instance of AudioProcessor and pass a reference
+		//to this object
+		AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+		processor.onBeat.AddListener(onOnbeatDetected);
+		processor.onSpectrum.AddListener(onSpectrum);
+	}
+	void Update()
+    {
+		transform.position += new Vector3(0f, 0f, 0.1f);
+	}
+	//this event will be called every time a beat is detected.
+	//Change the threshold parameter in the inspector
+	//to adjust the sensitivity
+	void onOnbeatDetected()
+	{
+
+		Vector3 newRWalllPos = new Vector3(rightWall.transform.position.x, rightWall.transform.position.y, transform.position.z);
+		GameObject newWall = Instantiate(rightWall, newRWalllPos, transform.rotation);
+		newWall.AddComponent<WallBehavior>();
+		//gameObject.active = !active;
+		active = !active;
+		UnityEngine.Debug.Log("Beat!!!");
+		//UnityEngine.Debug.Log(i);
+		i++;
+	}
+
+	//This event will be called every frame while music is playing
+	void onSpectrum(float[] spectrum)
+	{
+		//The spectrum is logarithmically averaged
+		//to 12 bands
+
+		for (int i = 0; i < spectrum.Length; ++i)
+		{
+			Vector3 start = new Vector3(i, 0, 0);
+			Vector3 end = new Vector3(i, spectrum[i], 0);
+			UnityEngine.Debug.DrawLine(start, end);
+		}
+	}
+
+}
+/*
+ * using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class BeatEffects : MonoBehaviour
+{
+	private int i;
+
+
+	private bool active;
+	public GameObject rightWall;
+	public GameObject leftWall;
+	void Start()
+	{
+		active = true;
+		i = 1;
+		//Select the instance of AudioProcessor and pass a reference
+		//to this object
+		AudioProcessor processor = FindObjectOfType<AudioProcessor>();
+		processor.onBeat.AddListener(onOnbeatDetected);
+		processor.onSpectrum.AddListener(onSpectrum);
+	}
+
+	//this event will be called every time a beat is detected.
+	//Change the threshold parameter in the inspector
+	//to adjust the sensitivity
+	void onOnbeatDetected()
+	{
+		Vector3 
+		Instantiate(leftWall, GameObject.Find("Squid").transform.position, transform.rotation);
+		//gameObject.active = !active;
+		active = !active;
+		UnityEngine.Debug.Log("Beat!!!");
+		UnityEngine.Debug.Log(i);
+		i++;
+	}
+
+	//This event will be called every frame while music is playing
+	void onSpectrum(float[] spectrum)
+	{
+		//The spectrum is logarithmically averaged
+		//to 12 bands
+
+		for (int i = 0; i < spectrum.Length; ++i)
+		{
+			Vector3 start = new Vector3(i, 0, 0);
+			Vector3 end = new Vector3(i, spectrum[i], 0);
+			UnityEngine.Debug.DrawLine(start, end);
+		}
+	}
+}*/
---
+++ Assets/Scripts/SquidMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class SquidMovement : MonoBehaviour
+{
+    private enum PlayerStatus { Right, Left, Moving };
+    private PlayerStatus squidStatus;
+    [SerializeField]
+    public float left = -4.15f;
+    [SerializeField]
+    public float right = 4.15f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        //Invoke("DelayStart", 5f); //Trying to make 5 second delay
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+        rb = gameObject.GetComponent<Rigidbody>();
+        squidStatus = PlayerStatus.Left;
+    }
+
+    IEnumerator DelayStart()
+    {
+        yield return new WaitForSeconds(5f);
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0f, 0f, 0.1f);
+        //Move left
+        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && (squidStatus == PlayerStatus.Right))
+        {
+            rb.AddForce(-10f, 0, 0, ForceMode.Impulse);
+            //Vector3 destination = new Vector3(left, transform.position.y, transform.position.z + 1f);
+            //StartCoroutine(Jump(1f, transform.position, destination, PlayerStatus.Left));
+        }
+
+        //Move right
+        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && (squidStatus == PlayerStatus.Left))
+        {
+            rb.AddForce(10f, 0, 0, ForceMode.Impulse);
+            //Vector3 destination = new Vector3(right, transform.position.y, transform.position.z + 1f);
+            //StartCoroutine(Jump(1f, transform.position, destination, PlayerStatus.Right));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rb.constraints = RigidbodyConstraints.FreezePositionY;
+    }
+
+    IEnumerator Jump(float waitTime, Vector3 start, Vector3 dest, PlayerStatus afterStatus)
+    {
+        squidStatus = PlayerStatus.Moving;
+        float i = 0f;
+        while (i < 1f)
+        {
+            transform.position = Vector3.Lerp(start, dest, i);
+            /*if (i < 0.5f)
+            {
+                transform.position = Vector3.Lerp(start, start + new Vector3(0, 2f, 0), i);
+            }*/
+            i += 0.2f;
+            yield return null;
+        }
+        squidStatus = afterStatus;
+        yield return 0;
+    }
+}
